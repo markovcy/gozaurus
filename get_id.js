a1efff19f88764e4a10dbe3f46fd386b7f271d8e
oleg <oleh.ovchynnikov@gmail.com>
2023-05-06 23:20:13 +0300

    parse_fibrous_filler

--- get_id.js
+++ get_id.js
@@ -53,4 +53,46 @@ function parse_disperse_filler(resp) {
     document.getElementsByName("chemical_nature")[1].placeholder = resp[0].chemical_nature
     document.getElementsByName("chemical_nature_iso")[0].placeholder = resp[0].chemical_nature_iso
     document.getElementsByName("marka")[0].placeholder = resp[0].marka
+    document.getElementsByName("chemical_nature_iso")[0].placeholder = resp[0].chemical_nature_iso
+    document.getElementsByName("marka")[0].placeholder = resp[0].marka
+}
+
+function parse_fibrous_filler(resp) {
+    console.log("Fibrous filler")
+    document.getElementsByName("marka")[0].placeholder = resp[0].marka
+    document.getElementsByName("density_iso")[1].placeholder = resp[0].density_iso
+    document.getElementsByName("density")[1].placeholder = resp[0].density
+    document.getElementsByName("linear_density")[0].placeholder = resp[0].linear_density
+    document.getElementsByName("linear_density_iso")[0].placeholder = resp[0].linear_density_iso
+    document.getElementsByName("length_of_fibres_iso")[0].placeholder = resp[0].length_of_fibres_iso
+    document.getElementsByName("length_of_fibres")[0].placeholder = resp[0].length_of_fibres
+    document.getElementsByName("filament_diameter")[0].placeholder = resp[0].length_of_fibres
+    document.getElementsByName("filament_diameter_iso")[0].placeholder = resp[0].length_of_fibres_iso
+    document.getElementsByName("tensile_strength")[1].placeholder = resp[0].tensile_strength
+    document.getElementsByName("tensile_strength_iso")[1].placeholder = resp[0].tensile_strength_iso
+    document.getElementsByName("tensile_modulus")[1].placeholder = resp[0].tensile_strength
+    document.getElementsByName("tensile_modulus_iso")[1].placeholder = resp[0].tensile_strength_iso
+    document.getElementsByName("elongation_break")[1].placeholder = resp[0].elongation_break
+    document.getElementsByName("elongation_break_iso")[1].placeholder = resp[0].elongation_break_iso
+    document.getElementsByName("moisture_content_iso")[0].placeholder = resp[0].moisture_content_iso
+    document.getElementsByName("moisture_content")[0].placeholder = resp[0].moisture_content
+}
+
+function parse_other_filler(resp) {
+    console.log("Other filler")
+    document.getElementsByName("density_iso")[1].placeholder = resp[0].density_iso
+    document.getElementsByName("density")[1].placeholder = resp[0].density
+    document.getElementsByName("moisture_content_iso")[0].placeholder = resp[0].moisture_content_iso
+    document.getElementsByName("moisture_content")[0].placeholder = resp[0].moisture_content
+    document.getElementsByName("purity_of_disperse_filler")[0].placeholder = resp[0].purity_of_disperse_filler
+    document.getElementsByName("purity_of_disperse_filler_iso")[0].placeholder = resp[0].purity_of_disperse_filler_iso
+    document.getElementsByName("dispersity")[0].placeholder = resp[0].dispersity
+    document.getElementsByName("dispersity_iso")[0].placeholder = resp[0].dispersity_iso
+    document.getElementsByName("chemical_nature")[1].placeholder = resp[0].chemical_nature
+    document.getElementsByName("chemical_nature_iso")[0].placeholder = resp[0].chemical_nature_iso
+    document.getElementsByName("marka")[0].placeholder = resp[0].marka
+    document.getElementsByName("chemical_nature_iso")[0].placeholder = resp[0].chemical_nature_iso
+    document.getElementsByName("marka")[0].placeholder = resp[0].marka
+    document.getElementsByName("content_filler")[0].placeholder = resp[0].content_filler
+    document.getElementsByName("content_filler_iso")[0].placeholder = resp[0].content_filler_iso
 }
