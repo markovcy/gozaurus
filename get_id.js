31b4964e3257309704d0738f1e1a23ad5cc458d5
oleg <oleh.ovchynnikov@gmail.com>
2023-05-13 22:04:18 +0300

    all fillers

--- get_id.js
+++ get_id.js
@@ -1,9 +1,10 @@
-let local = JSON.parse(localStorage.USER);
-let token1 = local["token"]
+
 let r
 let resp
 
 let Url = "http://164.68.125.44:42799/table/items/get?limit=1&lang=1&id=2"
+let local = JSON.parse(localStorage.USER);
+let token1 = local["token"]
 
 let response = await fetch(Url, {
     headers: {
@@ -48,25 +49,33 @@ if (id_type_material === 1) {
     console.log("Thermoplastic composite materials")
 }
 
+let filler_dispers = ["marka", "manufacturer", "chemical_nature", "chemical_nature_iso", "purity_of_disperse_filler", "purity_of_disperse_filler_iso", "dispersity", "dispersity_iso", "moisture_content", "moisture_content_iso", "density", "density_iso"]
 
-let filler_dispers = ["id_type_of_filler", "marka", "manufacturer", "chemical_nature", "chemical_nature_iso", "purity_of_disperse_filler", "purity_of_disperse_filler_iso", "dispersity", "dispersity_iso", "moisture_content", "moisture_content_iso", "density", "density_iso"]
+let filler_fibrous = ["marka", "manufacturer", "density", "density_iso", "linear_density_iso", "linear_density", "length_of_fibres_iso", "length_of_fibres", "filament_diameter", "filament_diameter_iso", "tensile_strength_iso", "tensile_strength", "tensile_modulus", "tensile_modulus", "elongation_break", "elongation_break_iso", "moisture_content", "moisture_content_iso"]
 
+let filler_dispers = ["marka", "manufacturer", "chemical_nature", "chemical_nature_iso", "purity_of_disperse_filler", "purity_of_disperse_filler_iso", "dispersity", "dispersity_iso", "moisture_content", "moisture_content_iso", "density", "density_iso", "content_filler_iso", "content_filler"]
 
-filler_dispers.forEach(function(item, i) {
-    let placeholder = resp[item]
-    console.log(placeholder)
-    fill_filler_dispers(item, placeholder)
-});
-
-
-
-function fill_filler_dispers(name, placeholder) {
+function fill_form(name, placeholder) {
     let selector = $(`input[name=${name}]:visible`)
-    console.log(selector)
-    console.log(name)
-    console.log(placeholder)
     selector[0].placeholder = placeholder
 }
+
+function parse_disperse_filler (resp, filler_dispers) {
+    filler_dispers.forEach(function(item, i) {
+        let placeholder = resp[item]
+        console.log(placeholder)
+        fill_form(item, placeholder)
+    });
+}
+
+function parse_fibrous_filler (resp, filler_fibrous) {
+    filler_fibrous.forEach(function(item, i) {
+        let placeholder = resp[item]
+        console.log(placeholder)
+        fill_form(item, placeholder)
+    });
+}
+
 
 
 
