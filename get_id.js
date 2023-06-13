5218a05c6794071230204d1c9ef0ebdbae6bb432
oleg <oleh.ovchynnikov@gmail.com>
2023-06-14 02:07:03 +0300

    flammability

--- get_id.js
+++ get_id.js
@@ -249,7 +249,7 @@ function parse_other_filler (resp) {
 
 function parse_TKM_disperse_filler (resp) {
     console.log("parse_TKM_disperse_filler")
-    let massivofplaceholders = ["surface_resistivity", "marka", "chemical_nature",  "content_reinforcement",  "density",  "mfr", "tensile_modulus",  "tensile_strength_break", "elongation_break", "charpy_notched_impact_strength", "charpy_impact_strength", "vicat", "melting_temperature"]
+    let massivofplaceholders = ["surface_resistivity", "marka", "chemical_nature",  "content_reinforcement",  "density",  "mfr", "tensile_modulus",  "tensile_strength_break", "elongation_break", "charpy_notched_impact_strength", "charpy_impact_strength", "vicat", "flammability", "melting_temperature"]
     let massivofvalue = ["id_content_filler", "content_reinforcement_iso", "density_iso","mfr_iso", "tensile_modulus_iso", "tensile_strength_break_iso", "elongation_break_iso",  "melting_temperature_iso"]
     do_foreach (resp, massivofplaceholders, massivofvalue)
 }
