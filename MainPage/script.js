8eb80be366d01de2fe47f4cedff143006fc8f9d3
oleg <oleh.ovchynnikov@gmail.com>
2023-07-12 10:04:37 +0300

    karusel

---
+++ MainPage/script.js
@@ -0,0 +1,26 @@
+// new Swiper(".swiper", {
+//     autoplay: {
+//         delay: 1000,
+//         stopOnLastSlide: true,
+//         disableOnInteraction: false
+//     }
+// })
+
+// new Swiper(".swiper-container")
+
+
+new Swiper('swiper-container', {
+    slidesPerView: 3,
+    grid: {
+      rows: 3,
+    },
+    mousewheel: {
+      forceToAxis: true,
+    },
+  });
+
+//   const swiper = new Swiper('.swiper', {
+//     autoplay: {
+//       delay: 5000,
+//     },
+//    });
