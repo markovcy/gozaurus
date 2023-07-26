1f74fc18ef1b8290dc6a8da6d0317593b3df8eb2
oleg <oleh.ovchynnikov@gmail.com>
2023-07-26 18:33:56 +0300

    удаление комментария

--- MainPage/script_mainpage.js
+++ MainPage/script_mainpage.js
@@ -5,33 +5,3 @@ $(document).ready(function() {
     autoplay:true
   });
 })
-
-
-
-
-// new Swiper(".swiper", {
-//     autoplay: {
-//         delay: 1000,
-//         stopOnLastSlide: true,
-//         disableOnInteraction: false
-//     }
-// })
-
-// new Swiper(".swiper-container")
-
-
-// new Swiper('swiper-container', {
-//     slidesPerView: 3,
-//     grid: {
-//       rows: 3,
-//     },
-//     mousewheel: {
-//       forceToAxis: true,
-//     },
-//   });
-
-//   const swiper = new Swiper('.swiper', {
-//     autoplay: {
-//       delay: 5000,
-//     },
-//    });
