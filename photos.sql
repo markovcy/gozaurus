667a8e28702333edc667040c7de07e16077d0e7f
oleg <oleh.ovchynnikov@gmail.com>
2023-08-13 20:40:21 +0300

    photos1

--- photos.sql
+++ photos.sql
@@ -1,2 +1,2 @@
-COPY items FROM '/docker-entrypoint-initdb.d/photos.csv' (HEADER, DELIMITER ',', FORMAT csv);
+COPY photos FROM '/docker-entrypoint-initdb.d/photos.csv' (HEADER, DELIMITER ',', FORMAT csv);
 select setval('photos_id_seq', (select max(id) from photos) + 1);
